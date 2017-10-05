c9194e7272b1db210dcb9c96feb5eeb961a4ac1d
Boris Pek <tehnick-8@yandex.ru>
2017-10-06 01:28:56 +0300

    Update get-js-dependecies.sh script:
    download source copy or javascript files.

--- get-js-dependecies.sh
+++ get-js-dependecies.sh
@@ -17,12 +17,12 @@ rm -rf js/lib
 mkdir -p js/lib
 cd js/lib
 
-wget -nv -c https://code.jquery.com/jquery-1.12.4.min.js                                             -O jquery.js
-wget -nv -c https://raw.github.com/JangoSteve/jQuery-EasyTabs/v3.2.0/lib/jquery.easytabs.min.js      -O jquery.easytabs.js
-wget -nv -c https://raw.github.com/pandell/jquery-hashchange/v1.3c/jquery.ba-hashchange.min.js       -O jquery.hashchange.js
+wget -nv -c https://code.jquery.com/jquery-1.12.4.js                                                 -O jquery.js
+wget -nv -c https://raw.github.com/JangoSteve/jQuery-EasyTabs/v3.2.0/lib/jquery.easytabs.js          -O jquery.easytabs.js
+wget -nv -c https://raw.github.com/pandell/jquery-hashchange/v1.3c/jquery.ba-hashchange.js           -O jquery.hashchange.js
 wget -nv -c https://raw.github.com/datagraph/jquery-jsonrpc/0.1.1/jquery.jsonrpc.js                  -O jquery.jsonrpc.js
 wget -nv -c https://raw.github.com/Cyntax/jquery-timer/0.1.1/jquery.plugin.js                        -O jquery.plugin.js
 wget -nv -c https://raw.github.com/Cyntax/jquery-timer/0.1.1/jquery.timer.js                         -O jquery.timer.js
-wget -nv -c https://raw.github.com/Mottie/tablesorter/v2.29.0/dist/js/jquery.tablesorter.min.js      -O jquery.tablesorter.js
-wget -nv -c http://requirejs.org/docs/release/2.3.5/minified/require.js                              -O require.js
+wget -nv -c https://raw.github.com/Mottie/tablesorter/v2.29.0/dist/js/jquery.tablesorter.js          -O jquery.tablesorter.js
+wget -nv -c http://requirejs.org/docs/release/2.3.5/comments/require.js                              -O require.js
 
