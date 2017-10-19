acb81c1328e1ed20b7d21bc4075ceb00ee0a132a
Boris Pek <tehnick-8@yandex.ru>
2017-10-20 02:09:32 +0300

    Simplify config file:
    there are enough documentation already.

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ define([], function () {
     'use strict';
     return {
         "jsonrpc" : {
-            "host" : "", // if empty it defaults to the host serving the script and falls back to localhost
+            "host" : "localhost",
             "port" : "3121"
         },
         "debugLevel" : 3
